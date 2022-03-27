39c8bf77d0cadbc69caa5beae59d75787bfa735a
Celdama <celdama.dev@gmail.com>
2022-03-27 09:58:57 +0200

    install tailwind & styled components

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,15 +1,16 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { Title } from './nav.tw';
 
 const Nav = () => {
   return (
-    <nav className="nav p-3 border-bottom">
-      <Link href="/" passHref>
-        <h2 className="pointer">Celdama</h2>
+    <nav className='nav p-3 border-bottom'>
+      <Link href='/' passHref>
+        <Title>Celdama</Title>
       </Link>
-      <Link href="/bio" passHref>
-        <p className="ms-5 pointer lead my-auto">Bio</p>
+      <Link href='/bio' passHref>
+        <p className='ms-5 pointer lead my-auto'>Bio</p>
       </Link>
     </nav>
-  )
-}
-export default Nav
+  );
+};
+export default Nav;
---
+++ components/Nav/nav.tw.js
@@ -0,0 +1,5 @@
+import tw from 'tailwind-styled-components';
+
+export const Title = tw.h2`
+  text-blue-400
+`;
