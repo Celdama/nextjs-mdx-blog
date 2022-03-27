4112a49276b67d099ba3020211e81422f818360c
Celdama <celdama.dev@gmail.com>
2022-03-27 09:08:44 +0200

    add first article to test logic of create page with nextjs

---
+++ pages/bio.js
@@ -0,0 +1,9 @@
+const Bio = () => {
+  return (
+    <div className="mt-3">
+      <p className="display-4 text-center">I'm Celdama</p>
+      <p className="text-center">Celdama's bio here</p>
+    </div>
+  )
+}
+export default Bio
