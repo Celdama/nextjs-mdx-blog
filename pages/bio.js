88232e18ba2c3d24b0bdfa61e911ec20b51b48b8
Celdama <celdama.dev@gmail.com>
2022-03-27 10:01:52 +0200

    remove starter files

--- pages/bio.js
+++ pages/bio.js
@@ -1,9 +1,9 @@
 const Bio = () => {
   return (
-    <div className="mt-3">
-      <p className="display-4 text-center">I'm Celdama</p>
-      <p className="text-center">Celdama's bio here</p>
+    <div className='mt-3'>
+      <p className='display-4 text-center'>I&apos;m Celdama</p>
+      <p className='text-center'>Celdama&apos;s bio here</p>
     </div>
-  )
-}
-export default Bio
+  );
+};
+export default Bio;
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,36 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import Link from 'next/link'
+import Head from 'next/head';
+import Image from 'next/image';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import Link from 'next/link';
 
 export default function Home({ posts }) {
   return (
-    <div className={styles.container}>
+    <div>
       {posts.map((post, index) => (
         <Link href={'/blog/' + post.slug} passHref key={index}>
-          <div className="card mb-3 pointer" style={{ maxWidth: '540px' }}>
-            <div className="row g-0">
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">{post.frontMatter.title}</h5>
-                  <p className="card-text">{post.frontMatter.description}</p>
-                  <p className="card-text">
-                    <small className="text-muted">{post.frontMatter.date}</small>
+          <div className='card mb-3 pointer' style={{ maxWidth: '540px' }}>
+            <div className='row g-0'>
+              <div className='col-md-8'>
+                <div className='card-body'>
+                  <h5 className='card-title'>{post.frontMatter.title}</h5>
+                  <p className='card-text'>{post.frontMatter.description}</p>
+                  <p className='card-text'>
+                    <small className='text-muted'>
+                      {post.frontMatter.date}
+                    </small>
                   </p>
                 </div>
               </div>
-              <div className="col-md-4 m-auto">
+              <div className='col-md-4 m-auto'>
                 <Image
                   src={post.frontMatter.thumbnailUrl}
-                  className="img-fluid mt-1 rounded-start"
-                  alt="thumbnail"
+                  className='img-fluid mt-1 rounded-start'
+                  alt='thumbnail'
                   width={500}
                   height={400}
-                  objectFit="cover"
+                  objectFit='cover'
                 />
               </div>
             </div>
@@ -37,22 +38,25 @@ export default function Home({ posts }) {
         </Link>
       ))}
     </div>
-  )
+  );
 }
 
 export const getStaticProps = async () => {
-  const files = fs.readdirSync(path.join('posts'))
-  const posts = files.map(filename => {
-    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
-    const { data: frontMatter } = matter(markdownWithMeta)
+  const files = fs.readdirSync(path.join('posts'));
+  const posts = files.map((filename) => {
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    );
+    const { data: frontMatter } = matter(markdownWithMeta);
     return {
       frontMatter,
-      slug: filename.split('.')[0]
-    }
-  })
+      slug: filename.split('.')[0],
+    };
+  });
   return {
     props: {
-      posts
-    }
-  }
-}
+      posts,
+    },
+  };
+};
