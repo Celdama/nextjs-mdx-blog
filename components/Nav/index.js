addbfdd50009bf7a57534970df27bada9ba88b12
Celdama <celdama.dev@gmail.com>
2022-03-28 09:13:25 +0200

    update navbar

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -4,12 +4,44 @@ import { Wrapper } from './nav.styles';
 const Nav = () => {
   return (
     <Wrapper>
-      <Link href='/' passHref>
-        <h2>Mon Blog</h2>
-      </Link>
-      <Link href='/bio' passHref>
-        <p className='ms-5 pointer lead my-auto'>Bio</p>
-      </Link>
+      <div className='header'>
+        <svg
+          fill='none'
+          stroke='currentColor'
+          viewBox='0 0 24 24'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth={2}
+            d='M4 6h16M4 12h16M4 18h16'
+          />
+        </svg>
+        <Link href='/' passHref>
+          <h2>Mon Blog</h2>
+        </Link>
+        <Link href='/bio' passHref>
+          <p className='ms-5 pointer lead my-auto'>Bio</p>
+        </Link>
+      </div>
+      <div className='footer'>
+        <input type='text' placeholder='Search ...' />
+        <svg
+          className='filter'
+          fill='none'
+          stroke='currentColor'
+          viewBox='0 0 24 24'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth={2}
+            d='M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4'
+          />
+        </svg>
+      </div>
       {/* <input type='search' name='' id='' /> */}
     </Wrapper>
   );
--- components/Nav/nav.styles.js
+++ components/Nav/nav.styles.js
@@ -6,8 +6,39 @@ export const Wrapper = styled.nav`
   background-color: #352f43;
   color: whitesmoke;
   display: flex;
-  align-items: center;
-  justify-content: space-between;
+  flex-direction: column;
+  /* align-items: center; */
+  /* justify-content: space-between; */
+
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .footer {
+    display: flex;
+    margin-top: 22px;
+    justify-content: space-between;
+
+    input {
+      padding: 12px;
+      color: whitesmoke;
+      width: 80%;
+      border: none;
+      background-color: #494355;
+    }
+
+    .filter {
+      background-color: #eeb395;
+      color: #352f43;
+      padding: 10px;
+    }
+  }
+
+  svg {
+    height: 24px;
+  }
 
   h2 {
     margin: 0;
