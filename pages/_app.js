e011d240eb40757d793cd9f8368cf0a494b9c60b
Celdama <celdama.dev@gmail.com>
2022-03-29 12:13:49 +0200

    pages: create GlobalStyle files

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import Nav from '../components/Nav';
 import 'normalize.css/normalize.css';
+import { GlobalStyle } from './GlobalStyle';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div style={{ backgroundColor: '#f9f9f9', height: '100vh' }}>
       <Nav />
       <Component {...pageProps} />
+      <GlobalStyle />
     </div>
   );
 }
