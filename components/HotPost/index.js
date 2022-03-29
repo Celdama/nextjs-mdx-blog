f9b655742542a86e1720f5ceb64f18ed03b49560
Celdama <celdama.dev@gmail.com>
2022-03-29 19:45:39 +0200

    components/HotPost/index: some destructuring

--- components/HotPost/index.js
+++ components/HotPost/index.js
@@ -10,19 +10,21 @@ import {
 import Link from 'next/link';
 
 export const HotPost = ({ hotPost }) => {
+  const { slug, frontMatter } = hotPost;
+  const { tags, title, date, thumbnailUrl } = frontMatter;
   return (
     <Wrapper>
       <h2>Hot post</h2>
-      <Link className='link' href={`/blog/${hotPost.slug}`} passHref>
+      <Link className='link' href={`/blog/${slug}`} passHref>
         <PostContent>
           <InfoContainer>
-            <Tag>{hotPost.frontMatter.tags[0]}</Tag>
-            <h5>{hotPost.frontMatter.title}</h5>
-            <p>{hotPost.frontMatter.date}</p>
+            <Tag>{tags}</Tag>
+            <h5>{title}</h5>
+            <p>{date}</p>
           </InfoContainer>
           <ImageContainer>
             <Image
-              src={hotPost.frontMatter.thumbnailUrl}
+              src={thumbnailUrl}
               width={290}
               height={170}
               alt='thumbnail'
