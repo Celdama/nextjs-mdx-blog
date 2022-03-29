815c327d8cdb631e86e100fc8d3ace8f93130083
Celdama <celdama.dev@gmail.com>
2022-03-29 07:41:44 +0200

    add Motivation & Mindset post

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['imgs.search.brave.com', 'd24f1whwu8r3u4.cloudfront.net'],
+    domains: [
+      'imgs.search.brave.com',
+      'd24f1whwu8r3u4.cloudfront.net',
+      '3.bp.blogspot.com',
+      'cdn.kastatic.org',
+      'simpleprogrammer.com',
+      'images.unsplash.com',
+      'cdn-media-1.freecodecamp.org',
+      '4.bp.blogspot.com',
+    ],
   },
 };
 
