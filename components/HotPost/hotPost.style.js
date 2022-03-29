1cfc2512d46f45cd6dde59a08d8ac57811c62d91
Celdama <celdama.dev@gmail.com>
2022-03-29 19:44:42 +0200

    components/HotPost: refactor HotPost style

--- components/HotPost/hotPost.style.js
+++ components/HotPost/hotPost.style.js
@@ -1,55 +1,55 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  font-family: sans-serif;
   width: 36rem;
+
   h2 {
     color: #2c2e42;
   }
+`;
 
-  .content {
-    height: 200px;
-    position: relative;
+export const PostContent = styled.article`
+  height: 200px;
+  position: relative;
 
-    :hover {
-      cursor: pointer;
-    }
-
-    .text {
-      width: 60%;
-      height: 130px;
-      position: absolute;
-      z-index: 2;
-      background-color: white;
-      right: 0;
-      padding: 16px;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      align-items: flex-start;
-    }
-
-    h5 {
-      font-size: 1rem;
-      color: #352f43;
-      margin: 0;
-    }
-
-    p {
-      margin: 0;
-      color: #6c757d;
-      font-size: 0.9rem;
-    }
-
-    .tag {
-      background-color: #eeb395;
-      padding: 6px 8px;
-      color: #352f43;
-    }
-
-    .thumb {
-      position: absolute;
-      bottom: 0;
-    }
+  h5 {
+    font-size: 1rem;
+    color: #352f43;
+    margin: 0;
   }
+
+  p {
+    margin: 0;
+    color: #6c757d;
+    font-size: 0.9rem;
+  }
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  width: 60%;
+  height: 130px;
+  position: absolute;
+  z-index: 2;
+  background-color: white;
+  right: 0;
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+export const ImageContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+`;
+
+export const Tag = styled.span`
+  background-color: #eeb395;
+  padding: 6px 8px;
+  color: #352f43;
 `;
--- components/HotPost/index.js
+++ components/HotPost/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Image from 'next/image';
-import { Wrapper } from './hotPost.style';
+import {
+  ImageContainer,
+  InfoContainer,
+  PostContent,
+  Tag,
+  Wrapper,
+} from './hotPost.style';
 import Link from 'next/link';
 
 export const HotPost = ({ hotPost }) => {
@@ -8,13 +14,13 @@ export const HotPost = ({ hotPost }) => {
     <Wrapper>
       <h2>Hot post</h2>
       <Link className='link' href={`/blog/${hotPost.slug}`} passHref>
-        <div className='content'>
-          <div className='text'>
-            <span className='tag'>{hotPost.frontMatter.tags[0]}</span>
-            <h5 className='title'>{hotPost.frontMatter.title}</h5>
+        <PostContent>
+          <InfoContainer>
+            <Tag>{hotPost.frontMatter.tags[0]}</Tag>
+            <h5>{hotPost.frontMatter.title}</h5>
             <p>{hotPost.frontMatter.date}</p>
-          </div>
-          <div className='thumb'>
+          </InfoContainer>
+          <ImageContainer>
             <Image
               src={hotPost.frontMatter.thumbnailUrl}
               width={290}
@@ -22,8 +28,8 @@ export const HotPost = ({ hotPost }) => {
               alt='thumbnail'
               objectFit='cover'
             />
-          </div>
-        </div>
+          </ImageContainer>
+        </PostContent>
       </Link>
     </Wrapper>
   );
