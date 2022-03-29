f661df8026a39fbf29c227a675b3d00e2460ae87
Celdama <celdama.dev@gmail.com>
2022-03-29 21:31:12 +0200

    components/Nav: refactor

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,20 +1,20 @@
 import Link from 'next/link';
-import { Wrapper } from './nav.styles';
+import { NavBottom, NavTop, SearchBar, Wrapper } from './nav.styles';
 
 const Nav = () => {
   return (
     <Wrapper>
       <div className='content'>
-        <div className='header'>
+        <NavTop>
           <Link href='/' passHref>
             <h2>Mon Blog</h2>
           </Link>
           {/* <Link href='/bio' passHref>
             <p className='ms-5 pointer lead my-auto'>Bio</p>
           </Link> */}
-        </div>
-        <div className='footer'>
-          <input type='text' placeholder='Search ...' />
+        </NavTop>
+        <NavBottom>
+          <SearchBar type='text' placeholder='Search ...' />
           <svg
             className='filter'
             fill='none'
@@ -29,7 +29,7 @@ const Nav = () => {
               d='M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4'
             />
           </svg>
-        </div>
+        </NavBottom>
       </div>
     </Wrapper>
   );
--- components/Nav/nav.styles.js
+++ components/Nav/nav.styles.js
@@ -1,63 +1,55 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.nav`
+  padding: 16px;
+  background-color: #352f43;
+  color: whitesmoke;
+
   .content {
     max-width: 40rem;
     margin: 0 auto;
     flex-direction: column;
-
     display: flex;
-  }
-
-  padding: 16px;
-  font-family: sans-serif;
-  background-color: #352f43;
-  color: whitesmoke;
-
-  .header {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .footer {
-    display: flex;
-    margin-top: 22px;
-    justify-content: space-between;
-    gap: 10px;
-
-    input {
-      flex-grow: 1;
-      padding: 12px;
-      color: whitesmoke;
-      /* width: 90%; */
-      outline: none;
-      border: none;
-      background-color: #494355;
-    }
-
-    .filter {
-      background-color: #eeb395;
-      color: #352f43;
-      padding: 10px;
-    }
-  }
-
-  svg {
-    height: 24px;
-  }
-
-  h2 {
-    margin: 0;
   }
 `;
 
-// Internet : c’est quoi ?
+export const NavTop = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-// Introduction au web.
+  h2 {
+    margin: 0;
 
-// Comment le web fonctionne ?
+    :hover {
+      cursor: pointer;
+    }
+  }
+`;
 
-// HTTP : c’est quoi ?
+export const NavBottom = styled.div`
+  display: flex;
+  margin-top: 22px;
+  justify-content: space-between;
+  gap: 10px;
 
-// HTML : c’est quoi ?
+  .filter {
+    background-color: #eeb395;
+    color: #352f43;
+    padding: 8px;
+    height: 24px;
+
+    :hover {
+      cursor: pointer;
+    }
+  }
+`;
+
+export const SearchBar = styled.input`
+  flex-grow: 1;
+  padding: 10px 12px;
+  color: whitesmoke;
+  outline: none;
+  border: none;
+  background-color: #494355;
+`;
