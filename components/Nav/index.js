4a1b7c887f923547ade6efb73b2ab04cbccd07b2
Celdama <celdama.dev@gmail.com>
2022-04-10 10:07:11 +0200

    components/Nav/index: comment searchBar until is functionnal

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -13,7 +13,7 @@ const Nav = () => {
             <p className='ms-5 pointer lead my-auto'>Bio</p>
           </Link> */}
         </NavTop>
-        <NavBottom>
+        {/* <NavBottom>
           <SearchBar type='text' placeholder='Search ...' />
           <svg
             className='filter'
@@ -29,7 +29,7 @@ const Nav = () => {
               d='M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4'
             />
           </svg>
-        </NavBottom>
+        </NavBottom> */}
       </div>
     </Wrapper>
   );
